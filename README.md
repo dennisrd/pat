5f4bd6e2c15d69e23ff395ccd150a242f256ff8b
Dennis Dams <dennis.dams@tno.nl>
2021-03-31 22:41:52 +0200

    Extensive changes, incl. better README

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
 # pat
-Pattern matching for Python values
+Pattern matching for Python values.
+
+A pattern is a value that may contain variables. Such a value stands for an arbitary sub-value. Example:
+```python
+>>> n = Var(int)
+>>> pat = Pattern([1, 2, n])
+>>> pat
+Pattern([1, 2, Var(<class 'int'>)])
+```
+This defines a pattern that is a list in which the 3rd element may be any integer value. Note how the variable n is defined: it is an instance of class Var, instantiated with a type (```int``` here).
+
+Pattern variables may be substituted by values:
+```python
+>>> pat.subst({n: 42})
+[1, 2, 42]
+```
+Method ```subst``` takes a *binding*, which is a dict that maps Vars to values, and returns the value obtained by performing the substitution. The pattern itself is not altered, so ```pat``` can be used again to produce another value by substitution:
+```python
+>>> pat.subst({n: 3})
+[1, 2, 3]
+```
+A pattern can also be used to *match* other values:
+```python
+>>> binding = pat.match([1, 2, 7])
+>>> binding
+{Var(<class 'int'>): 7}
+```
+Matching returns a binding. In this case, ```n``` is bound to the value 7.
+```python
+>>> binding[n]
+7
+>>> next(iter(binding)) is n  # check whether the first key in binding is the same object as n
+True
+```
+The value that is returned by method ```match``` is either ```None```, when there is no match, or a binding. This binding is such that when used as a substitution on the pattern, the result is equal to the value to be matched. So if ```p``` is a pattern and ```p.match(v)``` returns binding ```b```, then it will be the case that ```p.subst(b)``` equals ```v```. That is, ```p.subst(p.match(v)) == v```, if ```p.match(v)``` is not ```None```.
+
+If method ```match``` returns a dict (and not ```None```), its value is in fact an instance of class MatchDict, which inherits from the Python dict class, with one difference: the truth value of MatchDict value ```{}``` is True (and not False, as it would be for a value that is a direct instance of dict). This is so that we can use the result of a call to ```match``` as a condition in the natural way, even when the returned binding is empty:
+```python
+>>> ground_pattern = Pattern(['abc'])
+>>> match = ground_pattern.match(['abc'])
+>>> match
+{}
+>>> if match:
+...     print('they match')
+... else:
+...     print('they do not match')
+...
+they match
+```
+
+Pattern values can be constructed from Vars, numbers, and strings using list and dict containers.
+```python
+>>> pat = Pattern({'command': 'SetTicks', 'args': (arglist := Var(list))})
+>>> pat.match({'command': 'SetTicks', 'args': [2, 4, 8, 10]})[arglist]
+[2, 4, 8, 10]
+```
+Note the use of the Python walrus operator (:=) to name the ```Var(list)``` so that it can be referenced afterwards.
+
+A pattern may contain multiple Vars, and a substition does not need to bind all Vars.
+```python
+>>> x = Var(float); y = Var(float)
+>>> p = Pattern({'x_coord': x, 'y_coord': y})
+>>> p_x = p.subst({x: 3.14})
+>>> p_x
+{'x_coord': 3.14, 'y_coord': Var(<class 'float'>)}
+```
+Note that the result is not a Pattern, so in order to also substitute for ```y```, it needs to be wrapped in a Pattern constructor.
+```python
+>>> p_x.subst({y: 7.0})
+AttributeError: 'dict' object has no attribute 'subst'
+>>> Pattern(p_x).subst({y: 7.0})
+{'x_coord': 3.14, 'y_coord': 7.0}
+```
+
+The same Var may occur multiple times in a Pattern.
+```python
+>>> s = Var(str)
+>>> twice = Pattern([s, s])
+>>> twice.subst({s: 'one'})
+['one', 'one']
+>>> bool(twice.match(['one', 'two']))
+False
+>>> twice.match(['one', 'one'])
+{Var(<class 'str'>): 'one'}
+```
--- pat.py
+++ pat.py
@@ -1,224 +1,206 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jan 18 17:01:14 2021
-
-@author: Dennis Dams (dennis.dams@tno.nl)
-
-"""
-
-
 import unittest
 
 
-class Param:
-    """A named, typed parameter, to be used in match patterns"""
-    name: str
+class Var:
+    """A placeholder for a value of the specified type."""
     typ: type
 
-    def __init__(self, **name_type):
-        """Constructor expects 1 keyword argument, e.g. 'x=int'"""
-        if len(name_type.items()) > 1:
-            raise RuntimeError("multiple arguments to Param")
-        varname, vartype = next(iter(name_type.items()))
-        self.name = varname
-        self.typ = vartype
-
-    def __str__(self):
-        return 'Var(' + self.name + '=' + str(self.typ) + ')'
+    def __init__(self, typ=object):
+        self.typ = typ
 
     def __repr__(self):
-        return 'Var(' + self.name + '=' + str(self.typ) + ')'
+        return f'{self.__class__}({self.typ})'
+
+    def __str__(self):
+        return f'{self.__class__.__name__}({self.typ.__name__})'
+
+    def subst(self, bindings):
+        if self in bindings and isinstance(bindings[self], self.typ):
+            return bindings[self]
+        return self  # no binding for var, or type mismatch
+
+    def match(self, ground_val, current_bindings):
+        if not isinstance(ground_val, self.typ):
+            raise MatchException
+        if self not in current_bindings:
+            current_bindings[self] = ground_val
+        elif current_bindings[self] == ground_val:
+            return
+        else:
+            raise MatchException
+
+
+class MatchException(Exception):
+    pass
+
+
+class MatchDict(dict):
+    """Override bool() to also return True for empty dict."""
+    def __bool__(self):
+        return True
 
 
 class Pattern:
-    """A pattern is a Python value with parameters that stand for sub-values.
+    """A pattern is a Python value with variables that stand for sub-values.
 
     Allowed values are built from dict and list constructors,
     where the dict keys must be strings and the non-compound
-    values must be strings, numbers, booleans, or parameters of
+    values must be strings, numbers, booleans, or variables of
     one of these types."""
+
+    val: object
+
     def __init__(self, val):
         # TODO check val
-        self.pattern = val
-    
-    # TODO print Params
+        self.val = val
+
+    # TODO print Vars
     def __str__(self):
-        return 'Pattern(' + str(self.pattern) + ')'
-    
-    def subst(self, **bindings):
-        return Pattern(subst_any(self.pattern, bindings))
-    
+        return 'Pattern(' + str(self.val) + ')'
+
+    def __repr__(self):
+        return 'Pattern(' + str(self.val) + ')'
+
+    def subst(self, bindings):
+        #return Pattern.subst_any(self.val, bindings)
+        return self.subst_any(self.val, bindings)  # or: self.__class__
+
     def match(self, ground_val):
-        return match_any(self.pattern, ground_val)
+        bindings = {}
+        try:
+            self.match_any(self.val, ground_val, bindings)
+            return MatchDict(bindings)
+        except MatchException:
+            return None
 
+    #@staticmethod
+    @classmethod
+    def subst_any(cls, term, bindings):
+        """Dispatch according to type."""
+        if isinstance(term, Var):
+            return term.subst(bindings)
+        elif isinstance(term, dict):
+            return cls.subst_dict(term, bindings)
+        elif isinstance(term, list):
+            return cls.subst_list(term, bindings)
+        else:
+            return term  # substitution not implemented for values of other types
 
-def subst_any(term, bindings):
-    '''Dispatch according to type.'''
-    if isinstance(term, Param):
-        return subst_param(term, bindings)
-    elif isinstance(term, dict):
-        return subst_dict(term, bindings)
-    elif isinstance(term, list):
-        return subst_list(term, bindings)
-    else:
-        # substitution not implemented for values of other types
-        return term
+    @classmethod
+    #allow Var in key?
+    def subst_dict(cls, d: dict, bindings):
+        result = {}
+        for k, v in d.items():
+            result[k] = cls.subst_any(v, bindings)
+        return result
 
+    @classmethod
+    def subst_list(cls, lst: list, bindings):
+        return [cls.subst_any(el, bindings) for el in lst]
 
-def subst_param(var, bindings):
-    for k, val in bindings.items():
-        if var.name == k:
-            if var.typ == type(val):
-                return val
+    @staticmethod
+    def match_any(term, ground_val, bindings):
+        """Match base values, or dispatch according to container type."""
+        if ((isinstance(term, int) and isinstance(ground_val, int))
+            or
+            (isinstance(term, str) and isinstance(ground_val, str))):
+            if term == ground_val:
+                return
             else:
-                #print("type mismatch")
-                return var
-            break
-    # no binding for var
-    return var
-
-
-def subst_dict(d: dict, bindings):
-    #print(bindings)
-    result = {}
-    for k, v in d.items():
-        result[k] = subst_any(v, bindings)
-    return result
-
-
-def subst_list(l: list, bindings):
-    return [subst_any(el, bindings) for el in l]
-
-
-def match_any(term, ground_val):
-    """Dispatch according to type."""
-    if isinstance(term, Param):
-        return match_param(term, ground_val)
-    elif ((isinstance(term, int) and isinstance(ground_val, int))
-          or
-          (isinstance(term, str) and isinstance(ground_val, str))
-         ):
-        if term == ground_val:
-            return {}
+                raise MatchException
+        elif isinstance(term, Var):
+            term.match(ground_val, bindings)
+        elif isinstance(term, dict) and isinstance(ground_val, dict):
+            Pattern.match_dict(term, ground_val, bindings)
+        elif isinstance(term, list) and isinstance(ground_val, list):
+            Pattern.match_list(term, ground_val, bindings)
         else:
-            return None
-    elif isinstance(term, dict) and isinstance(ground_val, dict):
-        return match_dict(term, ground_val)
-    elif isinstance(term, list) and isinstance(ground_val, list):
-        return match_list(term, ground_val)
-    else:
-        return None
+            raise MatchException
 
+    @staticmethod
+    def match_dict(term: dict, ground_val: dict, bindings):
+        for k in term.keys():
+            # ground_val must at least have all keys that term has
+            if k not in ground_val:
+                raise MatchException
+            # if key is both in term and in ground_val, try to match the
+            # associated values
+            Pattern.match_any(term[k], ground_val[k], bindings)
 
-def match_param(var, ground_val):
-    if var.typ == type(ground_val):  #isinstance(ground_val, var.typ):
-        return { var.name : ground_val }
-    else:
-        return None
-
-
-def match_dict(term: dict, ground_val: dict):
-    bindings = {}
-    for k in term.keys():
-        # ground_val must at least have all keys that term has
-        if not k in ground_val:
-            return None
-        # if key is both in term and in ground_val, try to match the
-        # associated values
-        sub_bindings = match_any(term[k], ground_val[k])
-        if sub_bindings is None:
-            return None
-        else:
-            bindings.update(sub_bindings)
-    return bindings
-
-
-def match_list(l: list, ground_val: list):
-    bindings = {}
-    if len(l) != len(ground_val):
-        return None
-    for l_el, ground_val_el in zip(l, ground_val):
-        sub_bindings = match_any(l_el, ground_val_el)
-        if sub_bindings is None:
-            return None
-        else:
-            bindings.update(sub_bindings)
-    return bindings
+    @staticmethod
+    def match_list(lst: list, ground_val: list, bindings):
+        if len(lst) != len(ground_val):
+            raise MatchException
+        for l_el, ground_val_el in zip(lst, ground_val):
+            Pattern.match_any(l_el, ground_val_el, bindings)
 
 
 class TestPattern(unittest.TestCase):
 
-    @unittest.expectedFailure
-    def test0(self):
-        Param(x=int, y=str)
+    def test_Var(self):
+        i = Var(int)
+        x = Var()
 
-    def test1(self):
-        e = Pattern({'kind': 'Command',
-                     'method': 'Inc',
-                     'arg': Param(i=int)
-                     }
-                    )
-        # Pattern(kind='Command', method='Inc', ...)
-        # Inc(int i [in]) Inc(i=in(int))
-        e_subst = e.subst(i=43)
-        e_subst_expected = Pattern({'kind': 'Command',
-                                    'method': 'Inc',
-                                    'arg': 43})
-        #self.assertDictEqual(e_subst, e_subst_expected)
-        self.assertEqual(e_subst.pattern['kind'], e_subst_expected.pattern['kind'])
-        self.assertEqual(e_subst.pattern['method'], e_subst_expected.pattern['method'])
-        self.assertEqual(e_subst.pattern['arg'], e_subst_expected.pattern['arg'])
+    def test_pattern_subst(self):
+        v = Var(int)
+        e = Pattern(v)
+        e_subst = e.subst({v: 3})
+        self.assertEqual(e_subst, 3)
 
-    def test2(self):
-        #print(Pattern([Param(x=int)]).subst(x=0))
-        s = Pattern(Param(x=int))
-        s_match = s.match(3)
-        self.assertEqual(s_match['x'], 3)
+    def test_Var_subst(self):
+        i = Var(int)
+        i_subst = i.subst({i: 3})
+        self.assertEqual(i_subst, 3)
 
-    def test3(self):
-        d = Pattern({'aap': Param(y=int)})
-        d_match = d.match({'aap': 4})
-        self.assertEqual(d_match['y'], 4)
-        d_match = d.match({'aap': 4, 'noot': 'mies'})
-        self.assertEqual(d_match['y'], 4)
-        d_match = d.match({})
-        self.assertEqual(d_match, None)
-        d_match = d.match({'aap': Param(z=int)})
-        self.assertEqual(d_match, None)
+    # @unittest.expectedFailure
+    def test_Var_subst_type_mismatch(self):
+        i = Var(int)
+        i_subst = i.subst({i: 'a'})
+        self.assertEqual(i_subst, i)
 
-    def test4(self):
-        d = Pattern({'aap': Param(y=int), 'noot': Param(s=str)})
-        d_match = d.match({'aap': 4})
-        self.assertEqual(d_match, None)
-        d_match = d.match({'aap': 4, 'noot': 'mies'})
-        self.assertEqual(len(d_match), 2)
-        self.assertEqual(d_match['y'], 4)
-        self.assertEqual(d_match['s'], 'mies')
-        d_match = d.match({'aap': Param(z=int)})
-        self.assertEqual(d_match, None)
+    def test_pattern_list_subst(self):
+        i = Var(int)
+        e = Pattern([i])
+        e_subst = e.subst({i: 42})
+        self.assertEqual(e_subst, [42])
 
-    def test5(self):
-        d = Pattern([Param(y=int), Param(s=str), 6])
-        d_match = d.match([5, 'foo', 6])
-        self.assertEqual(len(d_match), 2)
-        self.assertEqual(d_match['y'], 5)
-        self.assertEqual(d_match['s'], 'foo')
-        d_match = d.match([5])
-        self.assertEqual(d_match, None)
+    def test_match_string(self):
+        match_result = Pattern('foo').match('foo')
+        self.assertEqual(match_result, {})
 
-    def test6(self):
-        d = Pattern([7])
-        d_match = d.match([7])
-        self.assertEqual(d_match, {})
-        d_match = d.match([5])
-        self.assertEqual(d_match, None)
+    def test_match_string_fail(self):
+        match_result = Pattern('foo').match(Pattern('bar'))
+        self.assertEqual(match_result, None)
 
-    def test7(self):
-        d = Pattern(['bar'])
-        d_match = d.match([7])
-        self.assertEqual(d_match, None)
-        d_match = d.match(['bar'])
-        self.assertEqual(d_match, {})
+    def test_match_int(self):
+        match_result = Pattern(7).match(7)
+        self.assertEqual(match_result, {})
+
+    def test_match_type_fail(self):
+        match_result = Pattern(7).match('foo')
+        self.assertEqual(match_result, None)
+
+    # @unittest.skip
+    def test_match_var(self):
+        match_result = Pattern(v := Var(int)).match(97)
+        # print(len(bindings))
+        # print(bindings[v])
+        # bindings == {v: 97}
+        self.assertEqual(len(match_result), 1)
+        self.assertEqual(match_result[v], 97)
+
+    def test_match_pattern_double_var_succeed(self):
+        x = Var(int)
+        e = Pattern({'arg1': x, 'arg2': x})
+        m = e.match({'arg1': 3, 'arg2': 3})
+        self.assertEqual(bool(m), True)
+        self.assertEqual(m[x], 3)
+
+    def test_match_pattern_double_var_fail(self):
+        x = Var(int)
+        e = Pattern({'arg1': x, 'arg2': x})
+        m = e.match({'arg1': 3, 'arg2': 4})
+        self.assertEqual(bool(m), False)
 
 
 if __name__ == '__main__':
